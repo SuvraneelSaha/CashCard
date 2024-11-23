c2580773cf11c4410a72eabc0e3f281f286d05b3
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2024-11-23 14:07:28 +0530

    created my first Test Case using jUnit ; in SpringBoot in Intellij ide

---
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,15 @@
+package com.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+    @Test
+    void myFirstTest(){
+        assertThat(42).isEqualTo(42);
+
+
+    }
+}
