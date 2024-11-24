8feef82ad7eb73da237950114484185431c86247
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-11-24 10:44:26 +0530

    trying from the of laptop
    Problem in tests , due to dependencies

--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+
 //import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 
